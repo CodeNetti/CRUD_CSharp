6fd26e070ae88249bd28ec3a94fedc3f905b662c
Raphael Simonetti <raphasimonettti@gmail,com>
2023-05-30 18:40:09 -0300

    ProjetO CRUD Finalizado

---
+++ bModel/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace bModel
+{
+    public class Usuario
+    {
+
+
+        
+        
+            public int IdUsu { get; set; }
+            public string NomeUsu { get; set; }
+            public string Cargo { get; set; }
+            public DateTime DataNasc { get; set; }
+        
+    }
+
+}
+
